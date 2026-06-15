59c89e8cae8c3951288453c5a8b7bfd4ec42f7d3
Andrew Cobb <andrew.cobb009@gmail.com>
2026-06-15 15:55:01 -0500

    Detects users location

---
+++ data/summaries.js
@@ -0,0 +1,27 @@
+masterSummaries = [
+  {
+    id: "clute-2026-6-11",
+    city: "Clute",
+    date: "2026-06-11",
+    summary: "The meeting talked about Clutians and pipes",
+    transcription: "We are here to talk ab Clutians and pipes!",
+    transcriptionLink: "https://www.youtube.com/",
+    absentees: [Bob]
+  }, {
+    id: "lake-Jackson-2026-6-11",
+    city: "Lake Jackson",
+    date: "2026-06-15",
+    summary: "Water is expesnive",
+    transcription: "Guys we need to fix our water price problem",
+    transcriptionLink: "https://www.youtube.com/",
+    absentees: []
+  }, {
+    id: "freeport-2026-6-11",
+    city: "Freeport",
+    date: "2026-06-22",
+    summary: "Freeport exists",
+    transcription: "I think, so maybe we are? Idk what freeport does",
+    transcriptionLink: "https://www.youtube.com/",
+    absentees: [John, Tyler]
+  }
+]
---
+++ scripts/buttons.js
@@ -0,0 +1,4 @@
+document.querySelector('.js-recent-meetings-button')
+  .addEventListener('click', () => {
+    window.location.href = "../webpages/summary.html"
+  });
