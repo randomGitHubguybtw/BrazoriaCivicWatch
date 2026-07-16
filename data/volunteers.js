e2c6420189f2d6b1783568c21a31c17d1fa722be
Andrew Cobb <andrew.cobb009@gmail.com>
2026-07-16 11:21:02 -0500

    Added volunteers

--- data/volunteers.js
+++ data/volunteers.js
@@ -2,5 +2,8 @@ export const masterVolunteers = [
   {
     email: "andrew.cobb009@gmail.com",
     role: "summarizer election_seeker seat_tracker"
+  }, {
+    email: "paypaypenney@gmail.com",
+    role: "summarizer"
   }
 ]
