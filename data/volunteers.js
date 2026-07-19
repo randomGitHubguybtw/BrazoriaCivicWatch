a1227b2d64bc12b8320df05716e2ff0f6c44066c
Andrew Cobb <andrew.cobb009@gmail.com>
2026-07-19 18:50:07 -0500

    Volunteer update

--- data/volunteers.js
+++ data/volunteers.js
@@ -3,7 +3,10 @@ export const masterVolunteers = [
     email: "andrew.cobb009@gmail.com",
     role: "summarizer election_seeker seat_tracker"
   }, {
+    email: "brazoriacivicwatch@gmail.com",
+    role: "summarizer election_seeker seat_tracker"
+  }, {
     email: "paypaypenney@gmail.com",
     role: "summarizer"
-  }
+  }, 
 ]
